4aab01d1dc49c6e041f2eccbd543dfa0814e4845
Manuel Rauber <manuel.rauber@thinktecture.com>
2020-06-14 20:25:37 +0200

    chore: add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Denon does not support command chaining yet, so we have to do it here.
+
+source .env
+
+denon bundle
+
+mkdir .deploy
+mv bot.js .deploy
+
+cd .deploy
+rsync -avzhe ssh --progress --delete ./* $REMOTE_SERVER:~/bot/.deploy
+
+rm -rf .deploy
