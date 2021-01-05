f5bf14a4cfe0d2645a7af781b04425b85b312505
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2021-01-05 12:31:52 +0100

    chore: bump version

--- src/BoundfoxStudios.DiscordBot/Modules/InfoModule.cs
+++ src/BoundfoxStudios.DiscordBot/Modules/InfoModule.cs
@@ -32,7 +32,7 @@ namespace BoundfoxStudios.DiscordBot.Modules
       // TODO: where do we get the version from?
       _logger.LogInformation("Setting status...");
 
-      var version = "v0.5.0";
+      var version = "v0.5.1";
 
 #if DEBUG
       version += "-debug";
