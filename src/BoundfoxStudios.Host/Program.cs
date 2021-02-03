5345ccddbfa5e86601a8a19c880e2219571918c3
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2021-02-03 09:00:25 +0100

    fix: include scope in logging

--- src/BoundfoxStudios.Host/Program.cs
+++ src/BoundfoxStudios.Host/Program.cs
@@ -13,9 +13,9 @@ namespace BoundfoxStudios.Host
     public static async Task Main(string[] args)
     {
       Log.Logger = new LoggerConfiguration()
-        .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext:l} {Message:lj}{NewLine}{Exception}")
+        .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext:l} {Scope} {Message:lj}{NewLine}{Exception}")
         .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day,
-          outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {SourceContext:l} {Message:lj}{NewLine}{Exception}")
+          outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {SourceContext:l} {Scope} {Message:lj}{NewLine}{Exception}")
         .Enrich.FromLogContext()
         .CreateLogger();
 
